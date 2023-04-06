344b3c1427610ac1b48813da43833d5f8a489d65
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2023-04-06 17:41:02 +0300

    Update tasksSlice.js

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,9 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { fetchTasks, addTask, deleteTask, toggleCompleted } from './operations';
-
-const handlePending = state => {
-  state.isLoading = true;
-};
 
 const handleRejected = (state, action) => {
   state.isLoading = false;
@@ -19,21 +15,21 @@ const tasksSlice = createSlice({
   },
   extraReducers: builder =>
     builder
-      .addCase(fetchTasks.pending, handlePending)
+
       .addCase(fetchTasks.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
         state.items = action.payload;
       })
       .addCase(fetchTasks.rejected, handleRejected)
-      .addCase(addTask.pending, handlePending)
+
       .addCase(addTask.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
         state.items.push(action.payload);
       })
       .addCase(addTask.rejected, handleRejected)
-      .addCase(deleteTask.pending, handlePending)
+
       .addCase(deleteTask.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
@@ -43,7 +39,7 @@ const tasksSlice = createSlice({
         state.items.splice(index, 1);
       })
       .addCase(deleteTask.rejected, handleRejected)
-      .addCase(toggleCompleted.pending, handlePending)
+
       .addCase(toggleCompleted.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
@@ -52,7 +48,18 @@ const tasksSlice = createSlice({
         );
         state.items.splice(index, 1, action.payload);
       })
-      .addCase(toggleCompleted.rejected, handleRejected),
+      .addCase(toggleCompleted.rejected, handleRejected)
+      .addMatcher(
+        isAnyOf(
+          fetchTasks.pending,
+          addTask.pending,
+          deleteTask.pending,
+          toggleCompleted.pending
+        ),
+        state => {
+          state.isLoading = true;
+        }
+      ),
 });
 
 export const tasksReducer = tasksSlice.reducer;

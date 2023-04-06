b095bbe2bb1a94b38f1f773f0ce770d49bbb8e0e
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2023-04-06 11:28:43 +0300

    start rerender

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,22 @@
-import { Layout } from 'components/Layout/Layout';
-import { AppBar } from 'components/AppBar/AppBar';
-import { TaskForm } from 'components/TaskForm/TaskForm';
-import { TaskList } from 'components/TaskList/TaskList';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTasks } from 'redux/operations';
+import { getTasks } from 'redux/selectors';
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+  const { items, isLoading, error } = useSelector(getTasks);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
   return (
-    <Layout>
-      <AppBar />
-      <TaskForm />
-      <TaskList />
-    </Layout>
+    <>
+      {isLoading && <p>Loading tasks...</p>}
+      {error && <p>{error}</p>}
+      <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p>
+    </>
   );
 };
--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,61 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { nanoid } from "nanoid";
+import { createSlice } from '@reduxjs/toolkit';
+// import { nanoid } from "nanoid";
 
-const tasksInitialState = [
-    { id: 0, text: "Learn HTML and CSS", completed: true },
-    { id: 1, text: "Get good at JavaScript", completed: true },
-    { id: 2, text: "Master React", completed: false },
-    { id: 3, text: "Discover Redux", completed: false },
-    { id: 4, text: "Build amazing apps", completed: false },
-  ];
+// const tasksInitialState = [
+//     { id: 0, text: "Learn HTML and CSS", completed: true },
+//     { id: 1, text: "Get good at JavaScript", completed: true },
+//     { id: 2, text: "Master React", completed: false },
+//     { id: 3, text: "Discover Redux", completed: false },
+//     { id: 4, text: "Build amazing apps", completed: false },
+//   ];
 
-  const tasksSlice = createSlice({
-    name: "tasks",
-    initialState: tasksInitialState,
-    reducers: {
-
-      addTask: {
-        reducer(state, action) {
-            return [...state, action.payload];
-        //   state.push(action.payload);
-        },
-        prepare(text) {
-          return {
-            payload: {
-              text,
-              id: nanoid(),
-              completed: false,
-            },
-          };
-        },
-      },
-
-      deleteTask(state, action) {
-        return state.filter(task => task.id !== action.payload);
-        // const index = state.findIndex(task => task.id === action.payload);
-        // state.splice(index, 1);
-      },
-
-      deleteOlCompleted(state) {
-        return state.filter(task => !task.completed);
-      },
-
-      toggleCompleted(state, action) {
-        return state.map(task => {
-            if (task.id !== action.payload) {
-              return task;
-            }
-            return { ...task, completed: !task.completed };
-          });
-        // for (const task of state) {
-        //   if (task.id === action.payload) {
-        //     task.completed = !task.completed;
-        //     break;
-        //   }
-        // }
-      },
+const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
+  reducers: {
+    fetchingInProgress(state) {
+      state.isLoading = true;
     },
-  });
 
-export const { addTask, deleteTask, toggleCompleted, deleteOlCompleted } = tasksSlice.actions;
+    fetchingSuccess(state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items = action.payload;
+    },
+
+    fetchingError(state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { fetchingInProgress, fetchingSuccess, fetchingError } =
+  tasksSlice.actions;
 export const tasksReducer = tasksSlice.reducer;
